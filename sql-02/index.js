7726fa02dd6076bee8597f8b6be460307195e7cb
Leonardo Casales <leonardo@smartnet.com.ar>
2022-01-12 22:33:14 -0300

    feat: select consulta

--- sql-02/index.js
+++ sql-02/index.js
@@ -1,3 +1,19 @@
 
 const { sequelize } = require('./database/sequelize');
 
+async function getAlumnos() {
+    try {
+        const alumnos = await sequelize.query('SELECT * FROM alumnos', { type: sequelize.QueryTypes.SELECT });
+        console.log(alumnos);
+    } catch (error) {
+        console.log("ERROR!!!: ",error)
+    }
+
+    // sequelize.query('SELECT * FROM alumnos', { type: sequelize.QueryTypes.SELECT })
+    //     .then( (alumnos) => {
+    //         console.log(alumnos);
+    //     })
+    //     .catch(error => console.log(error))
+}
+
+getAlumnos();
